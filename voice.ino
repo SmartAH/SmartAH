03c8bcabc49380d9b4f0adb381350422b049b4b9
nurfitriakh <nurfitriakh@gmail.com>
2016-05-16 22:24:16 +0700

    Update voice.ino

--- voice.ino
+++ voice.ino
@@ -1,25 +1,12 @@
 String voice;
 int 
-led1 = 8, //Connect LED 1 To Pin #2 
-led2 = 10, //Connect LED 2 To Pin #3 
-led3 = 12, //Connect LED 3 To Pin #4 
-led4 = 5, //Connect LED 4 To Pin #5 
-led5 = 6; //Connect LED 5 To Pin #6 
-
-void allon(){
-     digitalWrite(led1, HIGH); 
-     digitalWrite(led2, HIGH); 
-     digitalWrite(led3, HIGH); 
-     digitalWrite(led4, HIGH); 
-     digitalWrite(led5, HIGH); 
-}
-void alloff(){
-     digitalWrite(led1, LOW); 
-     digitalWrite(led2, LOW); 
-     digitalWrite(led3, LOW); 
-     digitalWrite(led4, LOW); 
-     digitalWrite(led5, LOW);
-}
+led1 = 8,
+led2 = 10, 
+led3 = 12, 
+led4 = 13,  
+led5 = 5;
+led6 = 6;
+led7 = 3;
 
 void setup() {
   Serial.begin(9600);
@@ -28,33 +15,74 @@ void setup() {
   pinMode(led3, OUTPUT); 
   pinMode(led4, OUTPUT); 
   pinMode(led5, OUTPUT); 
+  pinMode(led6, OUTPUT); 
+  pinMode(led7, OUTPUT); 
 }
-//-----------------------------------------------------------------------//  
+
+void hidupsemua(){
+     digitalWrite(led1, HIGH); 
+     digitalWrite(led2, HIGH); 
+     digitalWrite(led3, HIGH); 
+     digitalWrite(led4, HIGH); 
+     digitalWrite(led5, HIGH); 
+     digitalWrite(led6, HIGH); 
+     digitalWrite(led7, HIGH); 
+}
+void matisemua(){
+     digitalWrite(led1, LOW); 
+     digitalWrite(led2, LOW); 
+     digitalWrite(led3, LOW); 
+     digitalWrite(led4, LOW); 
+     digitalWrite(led5, LOW);
+     digitalWrite(led6, LOW);
+     digitalWrite(led7, LOW);
+}
+
+
 void loop() {
-  while (Serial.available()){  //Check if there is an available byte to read
-  delay(10); //Delay added to make thing stable 
-  char c = Serial.read(); //Conduct a serial read
-  if (c == '#') {break;} //Exit the loop when the # is detected after the word
-  voice += c; //Shorthand for voice = voice + c
+  while (Serial.available()){ 
+  delay(10); 
+  char c = Serial.read(); 
+  if (c == '#') {break;} 
+  voice += c; 
   }  
   if (voice.length() > 0) {
     Serial.println(voice); 
-//-----------------------------------------------------------------------//    
-  //----------Control Multiple Pins/ LEDs----------//  
-       if(voice == "*hidup") {allon();}  //Turn Off All Pins (Call Function)
-  else if(voice == "*mati"){alloff();} //Turn On  All Pins (Call Function)
-  
-  //----------Turn On One-By-One----------// 
-  else if(voice == "*TV") {digitalWrite(led1, HIGH);} 
-  else if(voice == "*kipas") {digitalWrite(led2, HIGH);}
-  else if(voice == "*AC") {digitalWrite(led3, HIGH);}
-  else if(voice == "*bedroom lights on") {digitalWrite(led4, HIGH);}
-  else if(voice == "*bathroom lights on") {digitalWrite(led5, HIGH);}
-  //----------Turn Off One-By-One----------// 
-  else if(voice == "*TV mati") {digitalWrite(led1, LOW);} 
-  else if(voice == "*kipas mati") {digitalWrite(led2, LOW);}
-  else if(voice == "*AC mati") {digitalWrite(led3, LOW);}
-  else if(voice == "*bedroom lights off") {digitalWrite(led4, LOW);}
-  else if(voice == "*bathroom lights off") {digitalWrite(led5, LOW);}
-//-----------------------------------------------------------------------//  
-voice="";}} //Reset the variable after initiating
+
+
+  if(voice == "*hidup") {
+       hidupsemua();}  
+  else if(voice == "*mati"){
+       matisemua();} 
+
+  else if(voice == "*TV") {
+       digitalWrite(led1, HIGH);} 
+  else if(voice == "*kipas") {
+       igitalWrite(led2, HIGH);}
+  else if(voice == "*AC") {
+       digitalWrite(led3, HIGH);}
+  else if(voice == "*dispenser") {
+       digitalWrite(led4, HIGH);}
+  else if(voice == "*sanyo") {
+       digitalWrite(led5, HIGH);}
+  else if(voice == "*lampu dapur") {
+       digitalWrite(led6, HIGH);}
+  else if(voice == "*lampu kamar surya") {
+       digitalWrite(led7, HIGH);}
+ 
+  else if(voice == "*TV mati") {
+       digitalWrite(led1, LOW);} 
+  else if(voice == "*kipas mati") {
+       digitalWrite(led2, LOW);}
+  else if(voice == "*AC mati") {
+       digitalWrite(led3, LOW);}
+  else if(voice == "*dispenser mati") {
+       digitalWrite(led4, LOW);}
+  else if(voice == "*sanyo mati") {
+       digitalWrite(led5, LOW);}
+  else if(voice == "*lampu dapur mati") {
+       digitalWrite(led6, LOW);}
+  else if(voice == "*lampu kamar surya mati") {
+       digitalWrite(led7, LOW);}
+
+voice="";}} 
