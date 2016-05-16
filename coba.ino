2ee692a7b9bfc90d690ac62b1652148454f8d5c0
SmartAH <smartah00@gmail.com>
2016-05-16 17:43:48 +0700

    Add files via upload

---
+++ coba.ino
@@ -0,0 +1,43 @@
+char kode;
+
+void setup() {
+  pinMode(12, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(8, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  kode = Serial.read();
+
+  if ( kode == '1')
+  {
+    digitalWrite(12, HIGH);
+  }
+  else if (kode == 'z')
+  {
+    digitalWrite(12, LOW);
+  }
+
+  if ( kode == '2')
+  {
+    digitalWrite(8, HIGH);
+  }
+  else if (kode == 'y')
+  {
+    digitalWrite(8, LOW);
+  }
+
+  if ( kode == '3')
+  {
+    digitalWrite(10, HIGH);
+  }
+  else if (kode == 'x')
+  {
+    digitalWrite(10, LOW);
+  }
+
+
+}
